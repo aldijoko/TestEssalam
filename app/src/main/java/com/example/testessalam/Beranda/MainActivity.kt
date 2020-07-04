6f02606e2280cc88a03128744d315e84636d5916
MuhammadAldiJokoSatr <aldi.perdana@myinfosys.net>
2020-07-04 13:22:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testessalam/Beranda/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testessalam.Beranda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testessalam.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/testessalam/Splash/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.testessalam.Splash
+
+object Constants {
+    val IS_FIRST_TIME = "is_first_time"
+    val APPLICATION_PREFS = "app_preference"
+}
